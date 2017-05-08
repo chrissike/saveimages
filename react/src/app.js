2f61145959f2e6b0c2e9526d09b7c68abe9180da
chrissike <christina.eidelloth@gmail.com>
2017-05-08 21:16:47 +0200

    Anpassungen Frontend

--- react/src/app.js
+++ react/src/app.js
@@ -84,41 +84,38 @@ class App extends React.Component {
             let extname = path.extname(item.pathSuffix);
             return extname == '.jpg' || extname == '.png' || extname == '.dng' || extname == '.webp';
         }).map(function (item, index) {
-            return <li key={index}>{item.pathSuffix}</li>
+            return <li className="list-group-item" key={index}>{item.pathSuffix}</li>
         });
 
         return (
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-xs-3">
+                    <div className="col-xs-4">
 
                         <div>
-                            <button id="openFile" className="btn btn-default">Upload image</button>
+                            <button id="openFile" className="btn-md btn-success">Upload image</button>
                         </div>
+                        
                         <div>
-                            <button id="downloadFile" className="btn btn-default">Download image</button>
-                        </div>
-                        <div>
-
-
-                        </div>
-                    </div>
-
-
-
-                    <div className="col-xs-6">
-                        <input type="text" onChange={this.filterEvent} ref={(input) => this.filterString = input} />
-                        <ul className="list-group-item-info">
+                            <input type="text" onChange={this.filterEvent} ref={(input) => this.filterString = input} />
+                        <ul className="list-group">
                             {listitems}
                         </ul>
 
                         <div className="row">
-                            <button id="refreshBtn" onClick={this.getImages} className="btn btn-default">Refresh</button>
+                            <button id="refreshBtn" onClick={this.getImages} className="btn-md btn-success">Refresh</button>
+                        </div>
+
+
                         </div>
                     </div>
 
-                    <div className="col-xs-3">
+                    
+                    <div className="col-xs-8">
                         Bild
+                     <div>
+                            <button id="downloadFile" className="btn-md btn-default-md">Download image</button>
+                     </div>   
                     </div>
 
 
